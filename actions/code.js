9cc11d980c413b84a84e7ed46d9dee2ee86d0ea6
Stephen Hyde <shh2112@gmail.com>
2022-04-18 11:33:49 -0400

    use new message format with data instead of body

--- actions/code.js
+++ actions/code.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const _ = require('lodash');
 const vm = require('vm');
-const { messages } = require('elasticio-node');
+const { messages } = require('../lib/messages');
 const co = require('co');
 const request = require('co-request');
 const moment = require('moment');
